f51f2a5e2f8a759b393bf3de6c05c50958e1b5b2
Grisselda Arcia <pazgriss3108@gmail.com>
2025-08-14 11:53:29 -0600

    commit del profe}

---
+++ IF2000_IntroduccionProgra/src/domain/Person.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author Griss
+ */
+public class Person {
+    //Atributes
+    private String name;
+    private String latName;
+    private String id;
+    private String phone;
+    private int age;
+
+    public Person(String name, String latName, String id, String phone, int age) {
+        this.name = name;
+        this.latName = latName;
+        this.id = id;
+        this.phone = phone;
+        this.age = age;
+    }
+    
+    public Person(){
+  
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLatName() {
+        return latName;
+    }
+
+    public void setLatName(String latName) {
+        this.latName = latName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "\n name=" + getName()
+                + "\n, latName=" + getLatName()
+                + "\n, id=" + getId()
+                + "\n, phone=" + getPhone()
+                + "\n, age=" + getAge();
+    }
+    
+    
+}
---
+++ IF2000_IntroduccionProgra/src/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introduccionprogra;
+
+/**
+ *
+ * @author Griss
+ */
+public class IF2000_IntroduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        //Data Types
+        String name = "Lionel"; 
+        int age = 40;
+                
+        System.out.println("Hello World");
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        
+        //Python:
+        //print('Hola Mundo')
+        
+        /*Tipos de dato numericos:
+        int, long, short, float, double numeric, money, decimal
+                
+        Manejo de fechas:
+        Date
+                
+        Cadena de caracteres:
+        string, char
+                
+        Objetos:
+        Object
+                
+                
+                
+                
+                */
+                
+    }
+    
+}
